5779044ea9c652d2022bc09f2c3c1b96170d019b
jack12 <970462711@qq.com>
2023-06-14 12:58:04 +0800

    ys bb test
    ys bb test

--- ys/assets/js/script.js
+++ ys/assets/js/script.js
@@ -17,7 +17,7 @@ window.WorkListTime = function( options ) {
     options.count               = options.hasOwnProperty('count') ? options.count : 0;
     options.works               = options.hasOwnProperty('works') ? options.works : document.querySelector("#js_html");
     options.alert               = options.hasOwnProperty('alert') ? options.alert : "今天是：";
-    options.timeErr             = options.hasOwnProperty('timeErr') ? options.timeErr : "";
+    options.timeErr             = options.hasOwnProperty('timeErr') ? options.timeErr : "请查询设定日期(" + options.fistWorkDay[0] + "年" + options.fistWorkDay[1] + "月" + options.fistWorkDay[2] + "日" + ")之后的日期";
     options.queryDate           = options.hasOwnProperty('queryDate') ? options.queryDate : document.querySelector("#inputDate");
     options.queryBtn            = options.hasOwnProperty('queryBtn') ? options.queryBtn : document.querySelector(".btn");
 
@@ -30,7 +30,7 @@ window.WorkListTime = function( options ) {
         options.month = options.date.getMonth() + 1;
         options.day = options.date.getDate();
 
-        options.timeErr = "请查询设定日期(" + options.fistWorkDay[0] + "年" + options.fistWorkDay[1] + "月" + options.fistWorkDay[2] + "日" + ")之后的日期";
+        // options.timeErr = "请查询设定日期(" + options.fistWorkDay[0] + "年" + options.fistWorkDay[1] + "月" + options.fistWorkDay[2] + "日" + ")之后的日期";
     
         that.query();
         that.workTime(options.fistWorkDay[0], options.fistWorkDay[1], options.fistWorkDay[2]);
